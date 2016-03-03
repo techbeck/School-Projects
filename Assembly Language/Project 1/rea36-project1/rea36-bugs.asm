d18b710440344fbc50b1a3a5e1fd2759634ed5b1
becca@addison.us <becca@addison.us>
2016-03-03 02:21:00 -0500

    updated queue functionality

--- Assembly Language/Project 1/rea36-project1/rea36-bugs.asm
+++ Assembly Language/Project 1/rea36-project1/rea36-bugs.asm
@@ -4,66 +4,125 @@ q:	.space	512
 	.text
 	li	$v0, 30
 	syscall
-	sw	$a0, initT
+	move	$s1, $a0		# time step
+	sw	$s1, initT
 	la	$k0, q+4
-	la	$k0, q
+	la	$k1, q
+	li	$s0, 31			# initialize player at center x
+	li	$a1, 63
+	li	$a2, 2
+	jal	_setLED
 loop:
+	# TO DO keypress stuff
 	li	$v0, 30
 	syscall
+	move	$s2, $a0		# current time
 	lw	$t0, initT
-	sub	$t1, $a0, $t0
-	slti	$t1, $t1, 20000
-	beq	$t1, $zero, endGame
-	jal	generateBug
-innerLoop:
-	slti	$t0, $a1, 64
-	beq	$t0, $zero, loop
-	jal	bugEvent
-	j 	innerLoop
+	sub	$t1, $s2, $t0
+	slti	$t1, $t1, 10000
+	beq	$t1, $zero, endGame	# ends game after time
+	sub	$t0, $s2, $s1
+	slti	$t0, $t0, 100
+	bne	$t0, $zero, loop
+	# TO DO different number generated depends on time
+	jal	_generateBug
+	
+	
+	li	$v0, 30
+	syscall
+	move	$s1, $a0		# new time step
 endGame:
 	li	$v0, 10
 	syscall
 
-	# generateBug()
+	# _insert_q(x,y,r,type)
+	# inserts event into queue at start and moves start
+	# arguments:
+	# $a0 = x
+	# $a1 = y
+	# $a2 = r
+	# $a3 = type (k = 0, b = 1, p = 2, w = 3)
+	# trashes: $t0
+_insert_q:
+	sb	$a0, 0($k0)
+	sb	$a1, 1($k0)
+	sb	$a2, 2($k0)
+	sb	$a3, 3($k0)
+	addi	$k0, $k0, 4
+	la	$t0, q+512
+	slt	$t0, $k0, $t0
+	bne	$t0, $zero, exitInsert
+	la	$k0, q
+exitInsert:
+	jr	$ra
+
+	# int _remove_q()
+	# removes event from queue at end and moves end
+	# returns: $v0 = address of event info
+	# trashes: $t0
+_remove_q:
+	la	$v0, 0($k1)
+	addi	$k1, $k1, 4
+	la	$t0, q+512
+	slt	$t0, $k1, $t0
+	bne	$t0, $zero, exitRemove
+	la	$k1, q
+exitRemove:
+	jr	$ra
+	
+	# _length()
+	# returns length of queue
+	# $v0 = length
+
+	# _generateBug()
 	# generates bug event and inserts into queue
+	# arguments:
 	# $a0 = x, set to random [0,63]
 	# $a1 = y, set to 0
-	# $a2 = r, set to 0
-	# $a3 = type, set to b
-generateBug:
+	# $a2 = radius, set to 0
+	# $a3 = type, set to 1
+	# trashes: none
+_generateBug:
 	addi	$sp, $sp, -4
 	sw	$ra, 0($sp)
 	li	$v0, 42
 	li	$a0, 0
 	li	$a1, 64
-	syscall
-	move	$a0, $a0
+	syscall				# get random number [0,63] for x
 	li	$a1, 0
 	li	$a2, 3
 	jal	_setLED
 	li	$a2, 0
-	li	$a3, 98
+	li	$a3, 1
+	jal	_insert_q
 	lw	$ra, 0($sp)
 	addi	$sp, $sp, 4
 	jr	$ra
 	
-
-
-	# bugEvent(x,y,0,b)
+	# _bugEvent(x,y,0,b)
 	# processes bug event
+	# arguments:
 	# $a0 = x
 	# $a1 = y, if not killed, $a1 set to $a1 + 1
 	# $a2 = 0, no radius for bugs
-	# $a3 = b, if killed, $a3 set to k
-	# trashes: none
-bugEvent:
+	# $a3 = 1, if killed, $a3 set to 0
+	# trashes: $t0
+_bugEvent:
 	addi	$sp, $sp, -4
 	sw	$ra, 0($sp)
-	li	$a2, 0		# turn off LED
+	li	$a2, 0			# turn off LED
 	jal	_setLED
 	addi	$a1, $a1, 1
-	li	$a2, 3		# turn on LED
+	jal	_getLED
+	beq	$v0, $zero, bugNextSpot
+	li	$a3, 3
+bugNextSpot:
+	li	$a2, 3			# turn on LED
 	jal	_setLED
+	slti	$t0, $a1, 64
+	bne	$t0, $zero, exitBug
+	li	$a3, 0			# kills bug if past screen
+exitBug:
 	li	$a2, 0
 	lw	$ra, 0($sp)
 	addi	$sp, $sp, 4
